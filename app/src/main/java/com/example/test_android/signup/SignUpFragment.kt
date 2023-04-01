40d1b82cd8c61b6a7714ff7fe2c990e13ff040ff
kimdahee7 <kikidahee7@duksung.ac.kr>
2023-04-01 16:49:49 +0900

    [feat] 회원가입 UI

---
+++ app/src/main/java/com/example/test_android/signup/SignUpFragment.kt
@@ -0,0 +1,11 @@
+package com.example.test_android.signup
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.test_android.R
+
+class SignUpFragment : Fragment() {
+}
