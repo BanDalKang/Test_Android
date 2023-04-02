0301f3bffd1ee4140d274a02ddeb021feccc7d0c
kimdahee7 <kikidahee7@duksung.ac.kr>
2023-04-02 22:58:17 +0900

    [refactor] Fragment -> Activity로 변경

---
+++ app/src/main/java/com/example/test_android/signup/SignUpActivity.kt
@@ -0,0 +1,12 @@
+package com.example.test_android.signup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.test_android.R
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
